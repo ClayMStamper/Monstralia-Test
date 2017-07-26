9d3c003b513965b1ff8d226a2d60e8267d7c2b19
Cstamper-PC\Cstamper <claystamper@gmail.com>
2017-07-26 17:00:19 -0500

    backing up half way through senses review production

---
+++ Monstralia-BrainstormLagoon/Assets/Scripts/ReviewGames/SensesReviewSenseButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensesReviewSenseButton : MonoBehaviour {
+    SensesReviewSenseItem item;
+
+    private void Start() {
+        item = FindObjectOfType<SensesReviewSenseItem>();
+    }
+    public void Touch() {
+        for (int i = 0; i < item.touchImages.Length; i++) {
+            if (item.touchImages[i] == )
+        }
+    }
+    public void Smell() {
+
+    }
+    public void Hear() {
+
+    }
+    public void Taste() {
+
+    }
+    public void See() {
+
+    }
+}
